73ca0092aa7c04ab545af29b0e530427e558ac2d
Haydee Gongora <16gongoruiz@gmail.com>
2026-07-04 17:20:29 -0500

    Actualizar configuración de API

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,6 @@
-import axios from 'axios'
+    import axios from 'axios'
 
-export const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000'
-})
+    export const api = axios.create({
+        //baseURL: 'http://127.0.0.1:8000'
+        baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000'
+    })
